463e4ed502851e1cfcf4fe2e1c468d6bfa8911ba
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2026-05-15 14:04:17 -0400

    fix navbar

--- src/components/layout/navbar/links/use-links.js
+++ src/components/layout/navbar/links/use-links.js
@@ -1,25 +1,5 @@
-import { useStaticQuery, graphql } from "gatsby"
-
 const useLinks = () => {
-  const query = useStaticQuery(
-    graphql`
-      query {
-        publications(list: { elemMatch: { title: { regex: "/.*/" } } }) {
-          list {
-            title
-          }
-        }
-      }
-    `
-  )
+  return ["home", "about", "cv", "research", "media"];
+};
 
-  const links = []
-
-  if (query.publications) {
-   links.push("home"), links.push("about"), links.push("cv"), links.push("research"), links.push("media")
-  }
-
-  return links
-}
-
-export default useLinks
+export default useLinks;
